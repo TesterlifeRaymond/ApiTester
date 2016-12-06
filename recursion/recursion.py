e832ee92031784d332a4b1ef6d7a2991586ca5dc
liujinjia <jinjialiu@creditease.cn>
2016-12-06 14:16:24 +0800

    * 修复当dict中value 类型为bool False 时 返回None的错误

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -22,16 +22,20 @@ def get_value(my_dict, key):
     """
 
     if isinstance(my_dict, dict):
-        if my_dict.get(key) or my_dict.get(key) == 0 or my_dict.get(key) == '':
+
+        if my_dict.get(key) or my_dict.get(key) == 0 or my_dict.get(key) == ''\
+                and my_dict.get(key) is False:
             return my_dict.get(key)
 
         for my_dict_key in my_dict:
-            if get_value(my_dict.get(my_dict_key), key):
+            if get_value(my_dict.get(my_dict_key), key) or \
+                            get_value(my_dict.get(my_dict_key), key) is False:
                 return get_value(my_dict.get(my_dict_key), key)
 
     if isinstance(my_dict, list):
         for my_dict_arr in my_dict:
-            if get_value(my_dict_arr, key):
+            if get_value(my_dict_arr, key) \
+                    or get_value(my_dict_arr, key) is False:
                 return get_value(my_dict_arr, key)
 
 
