fcc36c74c408c310f7d4b50ab2e8c831ab3a3c31
liujinjia <jinjialiu@creditease.cn>
2016-12-06 13:17:23 +0800

    *  json递归解析（单、多）参数包 整体优化完成

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -3,12 +3,16 @@
     这是一个递归函数的python包
     使用例子：
 
-    get_value(dict, get_values_key)
+    1 :get_value(dict, get_values_key)
 
-    data = {"code": 0, "message": "操作成功","result": {"amount": 950.0000,
-            "totalBaoLiFee": None, "pageNo": 1,"data": [{"goodsId": 100}]}}
+        data = {"code": 0, "message": "操作成功","result": {"amount": 950.0000,
+                "totalBaoLiFee": None, "pageNo": 1,"data": [{"goodsId": 100}]}}
 
-    print(get_value(data, "messages"))
+        print(get_value(data, "code"))
+
+
+    2: list_for_key_to_dict(*args, dict)
+        print(list_for_key_to_dict("code", "pageNo", "goodsId", my_dict=data))
 
 """
 
@@ -19,13 +23,29 @@ def get_value(my_dict, key):
 
     if isinstance(my_dict, dict):
         if my_dict.get(key) or my_dict.get(key) == 0 or my_dict.get(key) == '':
-            return my_dict[key]
+            return my_dict.get(key)
 
         for my_dict_key in my_dict:
-            if get_value(my_dict[my_dict_key], key):
-                return get_value(my_dict[my_dict_key], key)
+            if get_value(my_dict.get(my_dict_key), key):
+                return get_value(my_dict.get(my_dict_key), key)
 
     if isinstance(my_dict, list):
         for my_dict_arr in my_dict:
             if get_value(my_dict_arr, key):
                 return get_value(my_dict_arr, key)
+
+
+def list_for_key_to_dict(*args, my_dict):
+    """
+        接收需要解析的dict和 需要包含需要解析my_dict的keys的list
+
+    :param my_dict: 需要解析的字典
+    :param args: 包含需要解析的key的多个字符串
+        # list_for_key_to_dict("code", "pageNo", "goodsId", my_dict=dict)
+    :return: 一个解析后重新拼装的dict
+    """
+    result = {}
+    if len(args) > 0:
+        for key in args:
+            result.update({key: get_value(my_dict, key)})
+    return result
